1a77508da69bbf55cd6fb2e65c1012542290aa9d
israel-cr <israelcr2002@gmail.com>
2024-10-07 19:58:03 -0600

    Configuracion Inicial del Proyecto

---
+++ src/main/java/com/ArcaDeLaAlianza/ArcaDeLaAlianza/ArcaDeLaAlianzaApplication.java
@@ -0,0 +1,13 @@
+package com.ArcaDeLaAlianza.ArcaDeLaAlianza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcaDeLaAlianzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcaDeLaAlianzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArcaDeLaAlianza/ArcaDeLaAlianza/ArcaDeLaAlianzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArcaDeLaAlianza.ArcaDeLaAlianza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcaDeLaAlianzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
