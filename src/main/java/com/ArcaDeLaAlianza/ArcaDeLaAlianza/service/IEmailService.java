407438f1cc472b1c3eefbc86c25162dff07ac578
saul mora <kennykurosaki2000@gmail.com>
2024-12-03 19:20:57 -0600

    se borraron cosas inecesarias

--- src/main/java/com/ArcaDeLaAlianza/ArcaDeLaAlianza/service/IEmailService.java
+++
@@ -1,8 +0,0 @@
-package com.ArcaDeLaAlianza.ArcaDeLaAlianza.service;
-
-import com.ArcaDeLaAlianza.ArcaDeLaAlianza.service.models.EmailDTO;
-import jakarta.mail.MessagingException;
-
-public interface IEmailService {
-    public void sendEmail (EmailDTO email) throws MessagingException;
-}
