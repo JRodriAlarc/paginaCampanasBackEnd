10205ed5628b18f192df23efa9d58cb6c4d2b62c
saul mora <kennykurosaki2000@gmail.com>
2024-12-09 17:15:36 -0600

    obtener productos sin autenticacion

--- src/main/java/com/ArcaDeLaAlianza/ArcaDeLaAlianza/Config/SecurityConfig.java
+++ src/main/java/com/ArcaDeLaAlianza/ArcaDeLaAlianza/Config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
                     http.requestMatchers(HttpMethod.POST, "/auth/register").permitAll();
                     http.requestMatchers(HttpMethod.GET, "/bells").permitAll();
                     http.requestMatchers(HttpMethod.POST, "/orders").permitAll();
-                    http.requestMatchers(HttpMethod.POST, "/productOrders").permitAll();
+                    http.requestMatchers(HttpMethod.POST, "/products").permitAll();
                     http.anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
